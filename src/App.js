353243e81e6d7f03b9ae6ee33588b8edaa448716
100ke <bhy9803@gmail.com>
2023-11-29 17:59:14 +0900

    common settings

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+import './reset.scss';
+import './style.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. test test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='title'>가나다</h1>
+      <div className="container">dd</div>
     </div>
   );
 }
